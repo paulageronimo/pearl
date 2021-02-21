bb5c42488cf594ba3b8f76de9798c9afcc1451fd
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-02-21 07:20:05 +0000

    basic layout of the website. need to take in the url + breath pref variables

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
+# from beatCount import 
 
 # declare constants
 HOST = '0.0.0.0'
 PORT = 5000
+
+beats = 0
 
 # initialize flask application
 app = Flask(__name__)
@@ -14,17 +17,37 @@ Bootstrap(app)
 def home():
     return render_template("home.html")
 
-# @app.route('/start/')
-# def startx():
-#     return render_template("start.html")
-
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+  
 @app.route('/resources/')
 def resources():
     return render_template("resources.html")
 
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+#                                                actual app components
+@app.route('/start/')
+def start():
+    return render_template("start.html")
+
+
+
+#@app.route('/meditating/', methods=['GET', 'POST'])
+@app.route('/meditating/')
+def med():
+  # if request.method == "POST":
+  #   #beats = countBeats()
+  #   return redirect(url_for("process"))
+  return render_template("med.html")
+
+# @app.route('/process/', methods=['GET', 'POST'])
+# def process():
+#   if request.method == "POST":
+#     #if()
+#     #beats = countBeats()
+#     return redirect(url_for("process"))
+#   return render_template("process.html")
+
 
 # sample api endpoint
 @app.route('/api/test', methods=['GET', 'POST'])
@@ -40,6 +63,4 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(host=HOST,
-            debug=True,
-            port=PORT)
+    app.run(host=HOST, debug=True, port=PORT)
